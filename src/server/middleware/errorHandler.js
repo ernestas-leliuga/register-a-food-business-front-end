7a722261e71e989abd9184337157b6c39163694f
Simon Thomas <simon.thomas@civica.co.uk>
2022-03-18 16:01:54 +0000

    more debguging

--- src/server/middleware/errorHandler.js
+++ src/server/middleware/errorHandler.js
@@ -3,12 +3,13 @@ const PropsGenerator = require("../propsGenerator");
 
 const errorHandler = (err, req, res, next) => {
   error(err);
-  error(req.body);
-  error(req.originalUrl);
   error(`statusCode: ${res ? res.statusCode : err ? err.statusCode : null}`);
   var props = {
     statusCode: res ? res.statusCode : err ? err.statusCode : "500",
     err: err ? err : "An error occurred.",
+    errOutput1: "Req.body csrf: " + req.body._csrf,
+    errOutput2: "Req.originalUrl: " + req.originalUrl,
+    errOutput3: "Req.method: " + req.method,
     ...PropsGenerator(req)
   };
   if (err.message.match("template not found")) {
